4cd53a543446d5e9f32aa599b7b459e6e4de8999
UncleStriker <blm.cbrlock.nrnn@gmail.com>
2023-04-06 21:52:58 -0400

    clase 5

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+const cors = require('cors')
+app.use(cors())
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.get('/test',(request,response)=>{
+    response.send('testeando')
+})
+
+app.post('/postData',(request,response) =>{
+    console.log(request.body)
+    response.send(`Los datos enviados fueron:  ${request.body.email} ${request.body.password}`)
+})
+
+app.listen(3000,()=>{
+    console.log('Escuchando en el puerto 3000')
+})
