83c46cc3447283f433ae166800483abab24890d2
UncleStriker <blm.cbrlock.nrnn@gmail.com>
2023-04-13 21:57:49 -0400

    clase 7

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded())
-const cors = require('cors')
 app.use(cors())
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
+es6Renderer = require('express-es6-template-engine'),
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
 
 app.get('/test',(request,response)=>{
     response.send('testeando')
@@ -13,7 +23,26 @@ app.get('/test',(request,response)=>{
 
 app.post('/postData',(request,response) =>{
     console.log(request.body)
-    response.send(`Los datos enviados fueron:  ${request.body.email} ${request.body.password}`)
+    response.send(`Los datos enviados fueron ${request.body.email} ${request.body.password}`)
+})
+
+app.get('/index',(request,response) =>{
+    response.render('index')
+})
+app.post('/enviarDatos',async (request,response) =>{
+    pregunta = `existe algun lenguaje de programacion en las siguientes lineas?
+    "${request.body.buscar}". Responde true o false en minusculas`
+    console.log(pregunta);
+    const completion = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: pregunta
+      });
+      console.log(completion.data.choices[0].text);
+    if(completion.data.choices[0].text.trim() == "true"){
+        response.render('recibe_datos',{locals: {busqueda : "Busqueda invalida"}})
+    }else{
+        response.render('recibe_datos',{locals: {busqueda : request.body.buscar}})
+    }  
 })
 
 app.listen(3000,()=>{
